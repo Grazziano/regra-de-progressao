831d7f4a1701dd377f03a3216aa815c97a3a8aef
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2024-02-28 13:06:17 -0300

    feat: enter the grades obtained

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -16,18 +16,23 @@ public class App {
     System.out.println("Digite a quantidade de atividades para cadastrar:");
     int numberOfActivitiesToRegister = Integer.parseInt(scanner.nextLine());
 
-    String[] arrayDeString = new String[numberOfActivitiesToRegister];
-    int[] arrayDeInteiros = new int[numberOfActivitiesToRegister];
+    String[] activityNames = new String[numberOfActivitiesToRegister];
+    int[] weightActivities = new int[numberOfActivitiesToRegister];
+    int[] activityNotes = new int[numberOfActivitiesToRegister];
 
     for (int i = 0; i < numberOfActivitiesToRegister; i++) {
       System.out.format("Digite o nome da atividade %d:\n", i + 1);
-      String activityName = scanner.nextLine();
+      String currentActivityName = scanner.nextLine();
 
       System.out.format("Digite o peso da atividade %d:\n", i + 1);
-      int activityNote = Integer.parseInt(scanner.nextLine());
+      int currentActivityWeight = Integer.parseInt(scanner.nextLine());
 
-      arrayDeString[i] = activityName;
-      arrayDeInteiros[i] = activityNote;
+      System.out.format("Digite a nota obtida para %s:\n", currentActivityName);
+      int currentNote = Integer.parseInt(scanner.nextLine());
+
+      activityNames[i] = currentActivityName;
+      weightActivities[i] = currentActivityWeight;
+      activityNotes[i] = currentNote;
     }
   }
 }
