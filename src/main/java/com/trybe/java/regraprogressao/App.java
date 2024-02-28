35b7e7e36119970011dcb91a1e1d4e9a8b266cc7
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2024-02-28 12:51:46 -0300

    feat: register evaluation activities

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +11,23 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    int numberOfActivitiesToRegister = Integer.parseInt(scanner.nextLine());
+
+    String[] arrayDeString = new String[numberOfActivitiesToRegister];
+    int[] arrayDeInteiros = new int[numberOfActivitiesToRegister];
+
+    for (int i = 0; i < numberOfActivitiesToRegister; i++) {
+      System.out.format("Digite o nome da atividade %d:\n", i + 1);
+      String activityName = scanner.nextLine();
+
+      System.out.format("Digite o peso da atividade %d:\n", i + 1);
+      int activityNote = Integer.parseInt(scanner.nextLine());
+
+      arrayDeString[i] = activityName;
+      arrayDeInteiros[i] = activityNote;
+    }
   }
 }
